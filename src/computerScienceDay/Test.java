57b9d61a329abe51a7556404fcacab5bc64ad1ae
samiam567 <35432488+samiam567@users.noreply.github.com>
2019-02-12 08:45:51 -0500

    new

--- src/computerScienceDay/Test.java
+++ src/computerScienceDay/Test.java
@@ -1,9 +1,36 @@
 package computerScienceDay;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("ayyy whazzup  ya frikers");
+		boolean myBool = false;
+		
+		int myInt = 100000;
+		long myLong = 100000;
+		
+		float myFloat = 3.14f;
+		double myDouble = 3.14159;
+		
+		
+		
+		String myString = "hi";
+		char myChar = 'A';
+		
+		System.out.println(myString);
+		
+		Scanner key = new Scanner(System.in);
+		myString = key.next();
+		
+		System.out.println(myString);
+		
+		myString = myString + "1";
+		
+		
+		System.out.println(myString);
+		
+		
 	}
 
 }
